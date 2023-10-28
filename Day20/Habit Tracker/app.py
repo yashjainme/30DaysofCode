0af83612a785e6a04237c2506be19848f6563b80
Yash Jain <jain.yash20082004@gmail.com>
2023-10-28 14:30:52 +0530

    Add files via upload

---
+++ Day20/Habit Tracker/app.py
@@ -0,0 +1,23 @@
+# The design and project idea is not mine 
+# Credits to - udemy.com/user/josesalvatierra
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+habits = ["Test habit"]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', habits=habits, title='Habit Tracker - Home')
+
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_habit():
+    if request.method == 'POST':
+        habit = request.form.get('habit')
+        habits.append(habit)
+    return render_template('add_habit.html', title='Habit Tracker - Add Habit')
+    
---
+++ Day22/Habit Tracker/app.py
@@ -0,0 +1,41 @@
+# The design and project idea is not mine 
+# Credits to - udemy.com/user/josesalvatierra
+import datetime
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+habits = ["Test habit", "Test Habit 2"]
+
+@app.context_processor
+def add_calc_date_range():
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+        return dates
+    return {"date_range": date_range}
+
+@app.route('/')
+def index():
+    date_str = request.args.get("date")
+    if date_str:
+        selected_date = datetime.date.fromisoformat(date_str)
+    else:
+        selected_date = datetime.date.today()
+    return render_template('index.html',
+        habits=habits,
+        title='Habit Tracker - Home',
+    
+        selected_date=selected_date
+    )
+
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_habit():
+
+    if request.method == 'POST':
+        habit = request.form.get('habit')
+        habits.append(habit)
+    return render_template('add_habit.html', title='Habit Tracker - Add Habit', selected_date=datetime.date.today()
+   )
+    
